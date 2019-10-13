6e521f13844049240d3d02ce804c7b9f79ef5cd4
Oliver Nies <murp1149@gmail.com>
2019-10-13 22:22:55 +0200

    Added tests for the proxy generator class

---
+++ programmersdigest.Metamorphosis/Exceptions/MissingTriggerException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace programmersdigest.Metamorphosis.Exceptions
+{
+    public class MissingTriggerException : Exception
+    {
+        public MissingTriggerException(string message) : base(message) { }
+    }
+}
